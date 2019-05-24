8c5de7987d4127a218d1e7cf765ffb1098d22abf
almasantang <almasantang@gmail.com>
2019-05-24 13:54:39 +1000

    everything bismillah

---
+++ 5-interaction/assets/js/main.js
@@ -0,0 +1,37 @@
+$( document ).ready(function() {
+// start here  
+
+// menu slide  
+$('.menu-link').click(function() {
+  $('.menu').addClass('left');
+});
+
+$('.close-link').click(function() {
+  $('.menu').removeClass('left');
+});
+	
+
+
+// popup link
+$('.popup-link').click(function() {
+  $('.popup').addClass('active');
+});
+
+$('.popup').click(function() {
+  $('.popup').removeClass('active');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+// end here
+});
---
+++ on-demand/assets/js/slide.js
@@ -0,0 +1,27 @@
+$( document ).ready(function() {
+    
+
+$('.nav-item-1').click(function() {
+  $('.about').toggleClass('active');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
