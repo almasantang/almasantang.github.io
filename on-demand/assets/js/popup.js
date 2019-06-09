9f4b981f2a5bf4da9dd9d2ed1a290271659b4fc9
almasantang <almasantang@gmail.com>
2019-06-10 02:36:13 +1000

    final update

---
+++ on-demand/assets/js/popup.js
@@ -0,0 +1,116 @@
+$( document ).ready(function() {
+
+
+// start shape popup
+$('.l-1').click(function() {
+  $('.s-1').addClass('show');
+});
+// end shape popup
+
+// start shape popup
+$('.l-2').click(function() {
+  $('.s-2').addClass('show');
+});
+// end shape popup
+
+// start shape popup
+$('.l-3').click(function() {
+  $('.s-3').addClass('show');
+});
+// end shape popup
+
+// start shape popup
+$('.l-4').click(function() {
+  $('.s-4').addClass('show');
+});
+// end shape popup
+
+// start shape popup
+$('.l-5').click(function() {
+  $('.s-5').addClass('show');
+});
+// end shape popup
+
+// start shape popup
+$('.l-6').click(function() {
+  $('.s-6').addClass('show');
+});
+// end shape popup
+
+// start shape popup
+$('.l-7').click(function() {
+  $('.s-7').addClass('show');
+  $('.s-8').addClass('show');
+  $('.s-9').addClass('show');
+  $('.s-10').addClass('show');
+});
+// end shape popup
+
+// start shape popup
+$('.l-11').click(function() {
+  $('.s-11').addClass('show');
+});
+// end shape popup
+
+// start shape popup
+$('.l-12').click(function() {
+  $('.s-12').addClass('show');
+});
+// end shape popup
+
+// start shape popup
+$('.l-13').click(function() {
+  $('.s-13').addClass('show');
+});
+// end shape popup
+
+// start shape popup
+$('.l-14').click(function() {
+  $('.s-14').addClass('show');
+});
+// end shape popup
+
+// start shape popup
+$('.l-15').click(function() {
+  $('.s-15').addClass('show');
+});
+// end shape popup
+
+// start shape popup
+$('.l-16').click(function() {
+  $('.s-16').addClass('show');
+});
+// end shape popup
+
+// CLOSE ANY OPEN SHAPE
+$('.shape').click(function() {
+  $(this).removeClass('show');
+});
+// CLOSE ANY OPEN SHAPE
+
+// CLOSE ANY OPEN SHAPE
+$('.shape-2').click(function() {
+  $(this).removeClass('show');
+});
+// CLOSE ANY OPEN SHAPE
+
+// CLOSE ANY OPEN SHAPE
+$('.shape-3').click(function() {
+  $(this).removeClass('show');
+});
+// CLOSE ANY OPEN SHAPE
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
--- on-demand/assets/js/preload.js
+++ on-demand/assets/js/preload.js
@@ -1,4 +1,5 @@
 (function() {
+	
 	var preload = document.getElementById("preload");
 	var loading = 0;
 	var id = setInterval(frame, 64);
@@ -10,7 +11,7 @@
 		} else {
 			loading = loading + 1;
 			if(loading == 90) {
-				preload.style.animation = "fadeout 1s ease";
+				preload.style.animation = "fadeOut 1s ease";
 			}
 		}
 	}
